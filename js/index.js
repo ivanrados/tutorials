164100c323af88a5e0c6f329dc2142793479f0f7
Ivan Radoš <radosivan05@gmail.com>
2017-03-28 14:30:39 +0200

    Changed

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('.slider-container').flipster({
+    itemContainer: $('.slider'),
+    itemSelector: $('.slide'),
+    loop: true,
+    autoplay: 3000,
+    style: 'carousel',
+    spacing: -0.5,
+    scrollwheel: true,
+    buttons: false,
+})
